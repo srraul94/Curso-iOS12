102058de6ead6dc9d8450d8b1feca81e29897fe4
Raúl Sánchez <srraul94@gmail.com>
2019-11-25 13:18:27 +0100

    Inital Commit
    Initial Comiit

---
+++ iAmRich/iAmRich/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iAmRich
+//
+//  Created by Raúl  on 22/11/2019.
+//  Copyright © 2019 Raúl . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
