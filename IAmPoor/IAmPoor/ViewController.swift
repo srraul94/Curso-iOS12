adc579b757a45332d4dca32fa68e450cc33e2ba0
Raúl Sánchez <srraul94@gmail.com>
2019-11-27 01:40:28 +0100

    Final del proyecto - I Am Poor

--- IAmPoor/IAmPoor/ViewController.swift
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var imageViewLogo: UIImageView!
+    @IBOutlet weak var buttonEnriquecer: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func enriquecerme(_ sender: Any) {
+        self.labelTitle.text = "Soy rico!"
+        self.imageViewLogo.image = UIImage(named: "536072")
+    }
+    
 }
 
