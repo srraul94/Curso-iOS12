20e1f4bc9692264a3d8a0e01bb0ba2913ac8ca4d
Raúl Sánchez <srraul94@gmail.com>
2019-12-11 17:12:16 +0100

    Initial Commit Proyecto - NotesOnTheGo

---
+++ NotesOnTheGo/NotesOnTheGo/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesOnTheGo
+//
+//  Created by Raúl  on 11/12/2019.
+//  Copyright © 2019 Raúl . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
